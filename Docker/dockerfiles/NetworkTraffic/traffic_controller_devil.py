efb2d00aa06a19726d0c094f8283506d381f4692
Skully1011 <cbg1011oskar@gmail.com>
2023-12-11 19:06:46 -0800

    fixed typo in dockerfiles, adjusted timer limits

--- Docker/dockerfiles/NetworkTraffic/traffic_controller_devil.py
+++ Docker/dockerfiles/NetworkTraffic/traffic_controller_devil.py
@@ -2,8 +2,8 @@ import time, random
 from send_http import send_http_traffic
 from SYN_flood import syn_flood_attack
 
-LOWER_LIM = 0
-UPPER_LIM = 5.01
+LOWER_LIM = 3
+UPPER_LIM = 10.01
 WEB_NODE_IP="192.168.1.2"
 DEVIL_NODE_IP="192.168.1.5"
 
@@ -26,7 +26,7 @@ def main():
             session_cnt = random.randint(25,100)
             send_http_traffic(DEVIL_NODE_IP, WEB_NODE_IP, session_cnt, PCAP_FILE_NAME)
         else:
-            syn_flood_attack(WEB_NODE_IP, random.randint(100,10000))
+            syn_flood_attack(WEB_NODE_IP, random.randint(100,1000))
 
         # pick random amount of time to wait between next batch of traffic
         rand_float = random.uniform(LOWER_LIM, UPPER_LIM)
